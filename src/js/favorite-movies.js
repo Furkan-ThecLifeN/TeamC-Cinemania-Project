6736ca5f0455c0bfec147b7a345193cf09fb805b
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-04-07 20:17:20 +0300

    Favorilere eklenen film yoksa filmleri arama fonksiyonu yazıldı.

--- src/js/favorite-movies.js
+++ src/js/favorite-movies.js
@@ -1,8 +1,9 @@
-const container = document.getElementById('my-films');
+const container = document.getElementById('film-list');
 let page = 1;
 const loadBtn = document.querySelector('.load-btn');
 const APIKey = '0f552bbb3a7946c71382d336324ac39a';
 const library = '';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const library = JSON.parse(localStorage.getItem('library')) || [];
@@ -31,13 +32,13 @@ function displayMovies(movies) {
   movies.forEach(movie => {
     const movieCard = document.createElement('div');
     movieCard.innerHTML = `
-      <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
+      <img class="movie-image" alt="${movie.original_title}" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
       <h3>${movie.original_title}</h3>
       <p class="votes" >Vote/Votes  ${movie.vote_count}/${movie.vote_average}</p>
       <p class="popularity" >Populrity ${movie.popularity}</p>
       <p class="genre" >Genre ${movie.genre_ids}</p>
       <p class="about" >ABOUT ${movie.overview}</p>
-      <button type="button" class="removeBtn" data-id="${movie.id}">Remove From Library</button>`;
+      <button type="button" class="add-remove-btn" data-id="${movie.id}"></button>`;
     container.appendChild(movieCard);
   });
 }
@@ -45,21 +46,27 @@ function displayMovies(movies) {
 //user will see if there is no movie in library
 
 function showEmptyLibraryMessage() {
-  const message = document.getElementById('film-list');
-  const markup = `<p>OOOPS..<br>We are very sorry!You don’t have any movies at your library</p>
+  const message = document.getElementById('empty-message');
+  const markup = `<p class="markup">OOOPS..<br>We are very sorry!<br>You don’t have any movies at your library</p>
   <button type="button" class="searchBtn">Search Movie</button>`;
   message.innerHTML = markup;
-}
-loadBtn.addEventListener('click', () => {
-  page++;
+  const searchBtn = document.querySelector('.searchBtn');
+searchBtn.addEventListener('click', () => {
   fetchMovies();
 });
+}
+
 
 // Remove movie from library
-const removeBtn = document.querySelector('.removeBtn');
-removeBtn.addEventListener('click', () => {
-  const idToRemove = Number(removeBtn.dataset.id);
-  removeFromLibrary(idToRemove);
+const addRemoveBtn = document.querySelector('.add-remote-btn');
+addRemoveBtn.addEventListener('click', (e) => {
+  const checkId = e.target.dataset.id;
+  if(checkId.includes(library)){
+    addToLibrary()
+  }else{
+    removeFromLibrary(idToRemove);
+  }
+  
 });
 
 function removeFromLibrary(id) {
