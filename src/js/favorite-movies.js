537145f36ccf5179b48e37170594a59ad7aa9dc2
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-04-07 17:56:01 +0300

    boş kütüphane mesajı

--- src/js/favorite-movies.js
+++ src/js/favorite-movies.js
@@ -1,11 +1,17 @@
-import axios from 'axios';
-
 const container = document.getElementById('my-films');
 let page = 1;
 const loadBtn = document.querySelector('.load-btn');
 const APIKey = '0f552bbb3a7946c71382d336324ac39a';
 const library = '';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const library = JSON.parse(localStorage.getItem('library')) || [];
+  if (library.length === 0) {
+    showEmptyLibraryMessage();
+  } else {
+    displayMovies(library);
+  }
+});
 // see all movies when the page loaded
 async function fetchMovies() {
   try {
@@ -21,16 +27,10 @@ async function fetchMovies() {
 
 // list movies
 function displayMovies(movies) {
-  const libraryParse = JSON.parse(localStorage.getItem('library')) || [];
-
-  if (libraryParse.length === 0) {
-    showEmptyLibraryMessage();
-    return;
-  } else {
-    container.innerHTML = '';
-    movies.forEach(movie => {
-      const movieCard = document.createElement('div');
-      movieCard.innerHTML = `
+  container.innerHTML = '';
+  movies.forEach(movie => {
+    const movieCard = document.createElement('div');
+    movieCard.innerHTML = `
       <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
       <h3>${movie.original_title}</h3>
       <p class="votes" >Vote/Votes  ${movie.vote_count}/${movie.vote_average}</p>
@@ -38,18 +38,17 @@ function displayMovies(movies) {
       <p class="genre" >Genre ${movie.genre_ids}</p>
       <p class="about" >ABOUT ${movie.overview}</p>
       <button type="button" class="removeBtn" data-id="${movie.id}">Remove From Library</button>`;
-      container.appendChild(movieCard);
-    });
-  }
+    container.appendChild(movieCard);
+  });
 }
 
 //user will see if there is no movie in library
 
 function showEmptyLibraryMessage() {
-  const message = document.querySelector('.film-list');
-  message.innerHTML =
-    '<p>OOOPS..<br>We are very sorry!You don’t have any movies at your library</p>
-    <button type="button" class="searchBtn">Search Movie</button>';
+  const message = document.getElementById('film-list');
+  const markup = `<p>OOOPS..<br>We are very sorry!You don’t have any movies at your library</p>
+  <button type="button" class="searchBtn">Search Movie</button>`;
+  message.innerHTML = markup;
 }
 loadBtn.addEventListener('click', () => {
   page++;
@@ -69,5 +68,3 @@ function removeFromLibrary(id) {
   localStorage.setItem('library', JSON.stringify(updatedStorage));
   displayMovies(updatedStorage);
 }
-
-
