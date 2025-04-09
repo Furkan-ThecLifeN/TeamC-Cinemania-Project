0cfc454904e05ab42dd89f472a2102ebd9d930f9
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-04-09 22:06:49 +0300

    grid yapısı çalışıldı

--- src/js/favorite-movies.js
+++ src/js/favorite-movies.js
@@ -3,7 +3,28 @@ let page = 1;
 const loadBtn = document.querySelector('.load-btn');
 const APIKey = '0f552bbb3a7946c71382d336324ac39a';
 const library = '';
-
+const message = document.getElementById('empty-message');
+const genreMap = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const library = JSON.parse(localStorage.getItem('library')) || [];
@@ -29,44 +50,52 @@ async function fetchMovies() {
 // list movies
 function displayMovies(movies) {
   container.innerHTML = '';
+  message.innerHTML = '';
   movies.forEach(movie => {
     const movieCard = document.createElement('div');
+
     movieCard.innerHTML = `
-      <img class="movie-image" alt="${movie.original_title}" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
+      <img class="movie-image" alt="${
+        movie.original_title
+      }" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
       <h3>${movie.original_title}</h3>
-      <p class="votes" >Vote/Votes  ${movie.vote_count}/${movie.vote_average}</p>
+      
       <p class="popularity" >Populrity ${movie.popularity}</p>
-      <p class="genre" >Genre ${movie.genre_ids}</p>
-      <p class="about" >ABOUT ${movie.overview}</p>
-      <button type="button" class="add-remove-btn" data-id="${movie.id}"></button>`;
+      <p class="genre">${movie.genre_ids.map(id => genreMap[id]).join(',')}|${
+      movie.release_date.split('-')[0]
+    }</p>`;
     container.appendChild(movieCard);
   });
 }
 
+
+// <p class="votes" >Vote/Votes  ${movie.vote_count}/${movie.vote_average}</p>
+// <p class="about" >ABOUT ${movie.overview}</p>
+//Showing single movie card
+const filmCard = document.querySelectorAll('.movie-image');
+filmCard.addEventListener('click', () => {});
+
 //user will see if there is no movie in library
 
 function showEmptyLibraryMessage() {
-  const message = document.getElementById('empty-message');
   const markup = `<p class="markup">OOOPS..<br>We are very sorry!<br>You don’t have any movies at your library</p>
   <button type="button" class="searchBtn">Search Movie</button>`;
   message.innerHTML = markup;
   const searchBtn = document.querySelector('.searchBtn');
-searchBtn.addEventListener('click', () => {
-  fetchMovies();
-});
+  searchBtn.addEventListener('click', () => {
+    fetchMovies();
+  });
 }
-
 
 // Remove movie from library
 const addRemoveBtn = document.querySelector('.add-remote-btn');
-addRemoveBtn.addEventListener('click', (e) => {
+addRemoveBtn.addEventListener('click', e => {
   const checkId = e.target.dataset.id;
-  if(checkId.includes(library)){
-    addToLibrary()
-  }else{
+  if (checkId.includes(library)) {
+    addToLibrary();
+  } else {
     removeFromLibrary(idToRemove);
   }
-  
 });
 
 function removeFromLibrary(id) {
