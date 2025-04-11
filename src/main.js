10b6e28ff79fcf229423dc20ed0a268aec05dbc9
Furkan-ThecLifeN <business.furkankara.2006@gmail.com>
2025-04-11 20:18:38 +0300

    Sayfa yüklenmiyor bazı hatalar var

--- src/main.js
+++ src/main.js
@@ -3,17 +3,18 @@ import './js/pop-up-movie-card';
 import './js/pop-up-trailer-card';
 import './js/catalog';
 import './js/weekly-trends';
-import './js/footer';import { renderUpcomingSection } from './js/upcoming-this-month.js';
+import './js/footer';
+import { renderUpcomingSection } from './js/upcoming-this-month.js';
 
 renderUpcomingSection();
 
 export async function setup() {
-    await loadHTML('#header-placeholder', './partials/header.html');
-    await loadHTML('#hero-placeholder', './partials/hero.html');
-  }
+  await loadHTML('#header-placeholder', './partials/header.html');
+  await loadHTML('#hero-placeholder', './partials/hero.html');
+}
 
-  async function loadHTML(selector, url) {
-    const res = await fetch(url);
-    const html = await res.text();
-    document.querySelector(selector).innerHTML = html;
-  }
+async function loadHTML(selector, url) {
+  const res = await fetch(url);
+  const html = await res.text();
+  document.querySelector(selector).innerHTML = html;
+}
