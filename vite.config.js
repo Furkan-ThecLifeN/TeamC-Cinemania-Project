efff6bbea8afdcad91f076cf394cdb57a10409a8
Muhammet Burak Altun <altunburak88@gmail.com>
2025-04-13 23:57:08 +0300

    ain

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'; // Bu SATIR ŞART!
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/TeamC-Cinemania-Project/', // 👈 en üste taşıdık
     server: {
       host: '0.0.0.0',
     },
@@ -17,7 +18,6 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
       outDir: '../dist',
       emptyOutDir: true,
-      base: '/TeamC-Cinemania-Project/', // GitHub repo adı
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
